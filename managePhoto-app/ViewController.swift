7ed93f756350da86dc93ec31daa0491522110d4e
magnosith <magnosith@icloud.com>
2023-06-22 12:26:30 -0700

    Initial Commit

---
+++ managePhoto-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  managePhoto-app
+//
+//  Created by Student on 22/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
